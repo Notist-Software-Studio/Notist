37539e628dc19adec1ba8c84e0c142f50e134bbc
Bryan Hartono <bryan.h2001@yahoo.com>
2022-05-31 23:59:18 +0800

    added bottom and up navigation bar

--- app/src/main/java/com/example/notist/MainActivity.kt
+++ app/src/main/java/com/example/notist/MainActivity.kt
@@ -4,40 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.notist.ui.theme.NotistTheme
+import com.example.notist.bar.bottomNavigation
+import com.example.notist.bar.upNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotistTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                NotistApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NotistApp() {
+    NotistTheme {
+        Scaffold(
+            topBar = { upNavigation() },
+            bottomBar = { bottomNavigation() }
+        ) { padding -> 20.dp }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    NotistTheme {
-        Greeting("Android")
-    }
+    NotistApp()
 }
