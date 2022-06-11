6b963c4134a3b4d6df1191a269feaad26c090d26
edoardodominikus <edoardodominikus@gmail.com>
2022-06-11 19:32:12 +0800

    synchronized lazycolumn course with firestore data realtime

--- app/src/main/java/com/example/notist/MainViewModel.kt
+++ app/src/main/java/com/example/notist/MainViewModel.kt
@@ -1,30 +1,66 @@
 package com.example.notist
 
+import android.content.ContentValues
+import android.net.Uri
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.notist.data.dto.Course
+import com.example.notist.data.dto.Pdf
+import com.example.notist.data.dto.User
 import com.example.notist.data.service.CourseService
 import com.example.notist.data.service.ICourseService
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.launch
 
 class MainViewModel(var courseService: ICourseService = CourseService()) : ViewModel() {
+    val pdfs: ArrayList<Pdf> = ArrayList<Pdf>()
     var courses: MutableLiveData<List<Course>> = MutableLiveData<List<Course>>()
     private lateinit var firestore: FirebaseFirestore
+    private var storageReference = FirebaseStorage.getInstance().getReference()
+    var user : User? = null
+
     fun fetchCourses() {
+//        viewModelScope.launch {
+//            var innerCourses = courseService.fetchCourses()
+//            courses.postValue((innerCourses))
+//        }
         viewModelScope.launch {
-            var innerCourses = courseService.fetchCourses()
-            courses.postValue((innerCourses))
+            var courseCollection = firestore.collection("courses")
+            var courseListener = courseCollection.addSnapshotListener{
+                    querySnapshot, firebaseFirestoreException->
+                querySnapshot?.let{
+                        querySnapshot->
+                    val coursetemp: ArrayList<Course> = ArrayList<Course>()
+                    var documents = querySnapshot.documents
+                    coursetemp.clear()
+                    documents?.forEach{
+                        var course = it.toObject(Course::class.java)
+                        course?.let{
+                            coursetemp.add(it)
+                        }
+                        courses.value = coursetemp
+
+                    }
+                }
+            }
         }
+
     }
 
     init {
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
     }
+//    fun listenToCourses(){
+//        firestore.collection("courses").addSnapshotListener()
+//    }
 
     fun save(addCourse: Course) {
         val document = if (addCourse.courseId == null || addCourse.courseId.isEmpty()) {
@@ -34,7 +70,54 @@ class MainViewModel(var courseService: ICourseService = CourseService()) : ViewM
         }
         addCourse.courseId = document.id
         var handle = document.set(addCourse)
-        handle.addOnSuccessListener { Log.d("Firebase", "Document Saved") }
+        handle.addOnSuccessListener {
+            Log.d("Firebase", "Document Saved")
+            courses.value = courses.value
+            if(pdfs.isNotEmpty()){
+                uploadPDF()
+            }
+        }
         handle.addOnFailureListener { Log.e("Firebase", "Save failed $it") }
+    }
+
+    private fun uploadPDF() {
+        pdfs.forEach{
+            pdf ->
+            var uri = Uri.parse(pdf.localUri)
+            val pdfRef = storageReference.child("pdfs/${pdf.filename}")
+            val uploadTask  = pdfRef.putFile(uri)
+            uploadTask.addOnSuccessListener {
+                Log.i(ContentValues.TAG, "Image Uploaded $pdfRef")
+                val downloadUrl = pdfRef.downloadUrl
+                downloadUrl.addOnSuccessListener {
+                        remoteUri ->
+                    pdf.remoteUri = remoteUri.toString()
+                    updatePDFDatabase(pdf)
+
+                }
+            }
+            uploadTask.addOnFailureListener {
+                Log.e(ContentValues.TAG, it.message ?: "No message")
+            }
+        }
+    }
+
+    private fun updatePDFDatabase(pdf: Pdf) {
+        courses.let {
+               course ->
+            var pdfDocument = if (pdf.id.isEmpty()) {
+                firestore.collection("courses").document()
+            } else {
+                firestore.collection("files").document(pdf.id)
+            }
+            pdf.id = pdfDocument.id
+            var handle = pdfDocument.set(pdf)
+            handle.addOnSuccessListener {
+                Log.i(ContentValues.TAG, "Successfully updated pdf metadata")
+            }
+            handle.addOnFailureListener {
+                Log.e(ContentValues.TAG, "Error updating pdf data: ${it.message}")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/notist/presentation/courses/CourseScreen.kt
+++ app/src/main/java/com/example/notist/presentation/courses/CourseScreen.kt
@@ -1,6 +1,17 @@
 package com.example.notist.presentation.courses
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
+import android.provider.CalendarContract.Attendees.query
+import android.provider.CalendarContract.EventDays.query
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media.query
+import android.provider.MediaStore.Video.query
+import android.provider.OpenableColumns
 import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -21,13 +32,16 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.core.content.ContentResolverCompat.query
 import androidx.navigation.NavHostController
 import com.example.notist.MainViewModel
 import com.example.notist.R
 import com.example.notist.data.dto.Course
+import com.example.notist.data.dto.Pdf
 import com.example.notist.ui.theme.NotistTheme
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.lang.Character.getType
 
 val database = Firebase.database
 val myRef = database.getReference("message")
@@ -80,6 +94,7 @@ data class CourseStringPair(
 
 @Composable
 fun CourseBar(
+    courseId: String,
     class_name: String,
     major: String,
     modifier: Modifier = Modifier, navController: NavHostController
@@ -89,7 +104,7 @@ fun CourseBar(
         modifier = modifier
             .background(colorResource(id = R.color.light_blue), shape = RoundedCornerShape(10.dp))
             .width(370.dp)
-            .clickable { navController.navigate("Teacher/$class_name") },
+            .clickable { navController.navigate("Teacher/$courseId") },
         horizontalAlignment = Alignment.Start
     ) {
         Text(
@@ -119,12 +134,13 @@ fun AlignCourseBar(
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier
-            .paddingFromBaseline(top = 300.dp, bottom = 300.dp)
+            .paddingFromBaseline(bottom = 300.dp)
             .fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         items(data) { item ->
             CourseBar(
+                courseId = item.courseId,
                 class_name = item.class_name,
                 major = item.major,
                 modifier = Modifier,
@@ -207,8 +223,7 @@ fun HomeScreen(
                             ) {
                                 Button(
                                     onClick = {
-//                                        data.add(Course(class_name = class_name,
-//                                                major = major))
+
                                         var add_course = Course().apply {
                                             class_name = inClassName
                                             major = inMajor
@@ -235,7 +250,47 @@ fun HomeScreen(
                     }
                 }
             }
+            var pickedImageUri by remember { mutableStateOf<Uri?>(null) }
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                    val cursor = it.data?.data?.let { it1 ->
+                        context.contentResolver.query(
+                            it1,
+                            null,
+                            null,
+                            null,
+                            null
+                        )
+                    }
+                    val nameIndex = cursor?.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                    val sizeIndex = cursor?.getColumnIndex(OpenableColumns.SIZE)
+                    cursor?.moveToFirst()
+                    val fileName = nameIndex?.let { it1 -> cursor?.getString(it1) }
+                    val size = sizeIndex?.let { it1 -> cursor?.getLong(it1) }
+                    pickedImageUri = it.data?.data
+                    Toast.makeText(context, pickedImageUri.toString(), Toast.LENGTH_LONG).show()
+                    val pdf =
+                        fileName?.let { it1 -> Pdf(localUri = pickedImageUri.toString(), filename = it1) }
+                    if (pdf != null) {
+                        viewModel.pdfs.add(pdf)
+                    }
+                }
 
+//            Button(
+//                onClick = {
+//                    val intent = Intent(
+//                        Intent.ACTION_OPEN_DOCUMENT,
+//                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+//                    )
+//                        .apply {
+//                            addCategory(Intent.CATEGORY_OPENABLE)
+//                        }
+//                    launcher.launch(intent)
+//
+//                }
+//            ) {
+//                Text("Select")
+//            }
         }
     }
 }
